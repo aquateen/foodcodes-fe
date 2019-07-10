aed36b0594e104cb4a110c05613eaa1861b7dd55
Chris Johnson <chris@defself.com>
2019-07-09 17:38:09 -0700

    Updates

---
+++ precache-manifest.3fd52359594c12e7573bac52859426c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71c9568a22294b01a39ddff59d25ce1b",
+    "url": "/foodcodes-fe/index.html"
+  },
+  {
+    "revision": "cac396471632f9b01173",
+    "url": "/foodcodes-fe/static/css/main.a2c531ac.chunk.css"
+  },
+  {
+    "revision": "948d45152411c67d3817",
+    "url": "/foodcodes-fe/static/js/2.ab4009e2.chunk.js"
+  },
+  {
+    "revision": "cac396471632f9b01173",
+    "url": "/foodcodes-fe/static/js/main.8f22dd98.chunk.js"
+  },
+  {
+    "revision": "51d620d2d137d2fd4f6d",
+    "url": "/foodcodes-fe/static/js/runtime~main.9b3ab433.js"
+  },
+  {
+    "revision": "60f9bc876fca192cf0988e82fabaa3dc",
+    "url": "/foodcodes-fe/static/media/food.60f9bc87.jpg"
+  },
+  {
+    "revision": "544666ce0915cfbb55cd01448aa828d7",
+    "url": "/foodcodes-fe/static/media/postmates.544666ce.png"
+  }
+]);
