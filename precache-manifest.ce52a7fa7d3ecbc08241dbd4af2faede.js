1d434370d1ffdb26993fca94ddf8dbe0f415ed84
Chris Johnson <chris@defself.com>
2019-07-10 22:19:24 -0700

    Updates

---
+++ precache-manifest.ce52a7fa7d3ecbc08241dbd4af2faede.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e576ac2c150453e8676916e33f6973b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18c26dcb944a5873f7c3",
+    "url": "/static/css/main.f1ebcc57.chunk.css"
+  },
+  {
+    "revision": "948d45152411c67d3817",
+    "url": "/static/js/2.ab4009e2.chunk.js"
+  },
+  {
+    "revision": "18c26dcb944a5873f7c3",
+    "url": "/static/js/main.2858972f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "60f9bc876fca192cf0988e82fabaa3dc",
+    "url": "/static/media/food.60f9bc87.jpg"
+  },
+  {
+    "revision": "544666ce0915cfbb55cd01448aa828d7",
+    "url": "/static/media/postmates.544666ce.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/foodcodes-fe/precache-manifest.7cb76ee6bb16d17833a7252e1e143b1b.js"
+  "/precache-manifest.ce52a7fa7d3ecbc08241dbd4af2faede.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/foodcodes-fe/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
